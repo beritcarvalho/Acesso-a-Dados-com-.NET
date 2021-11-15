526e3768d1aad8f739dfb78350fc6a5197d8abb8
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-11-15 09:52:20 -0300

    ADO .NET
    Introdução ao ADO .NET

---
+++ DataAccess/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            connection.Close();
+            Console.WriteLine("Aberto e Fechado");
+
+            using (var connection2 = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+                connection2.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection2;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Name] FROM [Student]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID = {reader.GetGuid(0)} - Nome = {reader.GetString(1)}");
+                    }
+                }
+            }
+        }
+    }
+}
