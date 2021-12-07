d931dbea38230dc6dae1be29b505ab2b50d5c766
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-12-07 01:11:58 -0300

    Cadastro de usuário

--- Blog/Views/UserRegistration.cs
+++ Blog/Views/UserRegistration.cs
@@ -21,13 +21,78 @@ namespace Blog.Views
             Console.WriteLine("--------------------CADASTRAR NOVO USUÁRIO--------------------\n\n\n");
 
             var connection = new SqlConnection(connectionString);
-
-            connection.Open();
             var repository = new Repository<User>(connection);
+
+            var user = CreateUser();
+
+            connection.Open();           
+            repository.Create(user);
             connection.Close();
+
+            Console.WriteLine("Ir para o menu");
             Console.ReadKey();
 
             MenuMain.Show();
+        }
+
+        public static User CreateUser()
+        {
+            var user = new User();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Informações do Perfil");
+
+                Console.Write("\tNome: ");
+                user.Name = Console.ReadLine();
+
+                Console.Write("\tEmail: ");
+                user.Email = Console.ReadLine();
+
+                Console.Write("\tBiografia: ");
+                user.Bio = Console.ReadLine();
+
+                Console.Write("\tUrl da Imagem: ");
+                user.Image = Console.ReadLine();
+
+                Console.Write("\tUrl do Usuario: ");
+                user.Slug = Console.ReadLine();
+
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\tCadastro de Senha");
+                    Console.Write("\t\tNova senha: ");
+                    var password = Console.ReadLine();
+
+                    Console.Write("\t\tConfirme a senha: ");
+                    var passwordConfirmation = Console.ReadLine();
+
+                    if (password == passwordConfirmation)
+                    {
+                        user.PasswordHash = password;
+                        break;
+                    }
+                }
+
+                Console.Clear();
+                Console.WriteLine("\tConfirmar? 1 - SIM");
+
+                Console.WriteLine($"\t\tNome: {user.Name}");
+                Console.WriteLine($"\t\tEmail: {user.Email}");
+                Console.WriteLine($"\t\tBiografia: {user.Bio}");
+                Console.WriteLine($"\t\tUrl Imagem: {user.Image}");
+                Console.WriteLine($"\t\tLink de usuário: {user.Slug}");
+                Console.WriteLine($"\t\tSenha: {user.PasswordHash}");
+
+                var choise = Console.ReadLine();
+                if (choise == "1")
+                    break;
+
+            }
+            return user;
         }
     }
 }
